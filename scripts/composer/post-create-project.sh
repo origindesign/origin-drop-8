1e159b1c6a46e133151fcfa114a8d1ce0b787013
Lindsay Wilson <lwilson@origindesign.ca>
2018-06-25 17:03:59 -0700

    updated create-project bash script

--- scripts/composer/post-create-project.sh
+++ scripts/composer/post-create-project.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 docker-compose run -w /var/www/html/web php drush site-install standard --db-url=mysql://drupal:drupal@mariadb:3306/drupal --yes --strict=0
-docker-compose run -w /var/www/html/web php drush pm-enable config_direct_save simple_block responsive_image datetime_range ctools metatag token components admin_toolbar admin_toolbar_tools devel features linkit paragraphs smart_trim pathauto field_group address inline_entity_form entity_browser entity_browser_entity_form media_entity_browser twig_field_value hsts link_attributes menu_block redirect redis role_delegation video_embed_field view_unpublished --yes --strict=0
+docker-compose run -w /var/www/html/web php drush pm-enable config_direct_save simple_block responsive_image datetime_range ctools metatag token components admin_toolbar admin_toolbar_tools devel features linkit paragraphs smart_trim pathauto field_group address inline_entity_form entity_browser entity_browser_entity_form media_entity_browser twig_field_value hsts link_attributes menu_block redirect redis role_delegation video_embed_field view_unpublished origin_extend --yes --strict=0
 docker-compose run -w /var/www/html/web php drush pm-enable efq helper includer --yes --strict=0
 docker-compose run -w /var/www/html/web php drush pm-enable origin --yes --strict=0
 docker-compose run -w /var/www/html/web php drush pm-enable global --yes --strict=0
