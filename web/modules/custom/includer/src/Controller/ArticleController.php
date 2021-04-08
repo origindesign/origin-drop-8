4385ddb80e700ae731c9420b85cd7211e83d205c
Lindsay Wilson <lwilson@origindesign.ca>
2021-04-08 10:33:18 -0700

    theme updates

--- web/modules/custom/includer/src/Controller/ArticleController.php
+++ web/modules/custom/includer/src/Controller/ArticleController.php
@@ -16,69 +16,71 @@ class ArticleController extends ContentController {
 
 
 
-    public function getIncludes () {
+  public function getIncludes () {
 
-        $this->result["listing_ajax"] = $this->getNodes($this->arguments["tag"]);
-        $this->result["filter_ajax"] = $this->createFilters();
-        $this->result["additional_classes"] = 'article';
+    $this->result["listing_ajax"] = $this->getNodes($this->arguments["tag"]);
+    $this->result["filter_ajax"] = $this->createFilters();
+    $this->result["additional_classes"] = 'article';
 
-        /*if(isset($this->arguments["tag"]) && $this->arguments["tag"] != ''){
-            $this->result["listing_heading"] = '<h2><span>Tag: '.$this->arguments["tag"].'</span></h2>';
-        }*/
+    /*if(isset($this->arguments["tag"]) && $this->arguments["tag"] != ''){
+        $this->result["listing_heading"] = '<h2><span>Tag: '.$this->arguments["tag"].'</span></h2>';
+    }*/
 
-        return $this->result;
+    return $this->result;
 
+  }
+
+
+  protected function getNodes ($tag){
+
+    $category = 'all';
+
+    if($tag != ''){
+      $term = \Drupal::entityTypeManager()
+        ->getStorage('taxonomy_term')
+        ->loadByProperties(['name' => $tag, 'vid' => 'article_category']);
+      $term = reset($term);
+      $category = 'field_category--'.$term->id().'';
     }
 
+    $defaultParams = '{"content_type":"article","category":"'.$category.'","paged":"1-8--restricted-5","sort":"created-DESC"}';
 
-    protected function getNodes ($tag){
+    return [
+      '#type' => 'markup',
+      '#markup' => "<div class='ajax-list-container ajax-articles loading' data-ajax-path='/efq/post' data-default-params='".$defaultParams."'></div>"
+    ];
 
-        if($tag != ''){
-            $term = \Drupal::entityTypeManager()
-                ->getStorage('taxonomy_term')
-                ->loadByProperties(['name' => $tag, 'vid' => 'article_category']);
-            $term = reset($term);
-            $defaultParams = '{"content_type":"article","category":"field_category--'.$term->id().'","paged":"1-10--restricted-5","sort":"created-DESC"}';
-        }else{
-            $defaultParams = '{"content_type":"article","category":"all","paged":"1-10--restricted-5","sort":"created-DESC"}';
-        }
-
-        return [
-            '#type' => 'markup',
-            '#markup' => "<div class='ajax-list-container ajax-articles loading' data-ajax-path='/efq/post' data-default-params='".$defaultParams."'></div>"
-        ];
-
-    }
+  }
 
 
 
-    protected function createFilters(){
+  protected function createFilters(){
 
-        $categories = $this->getTermsFromVocabulary('article_category', false, 'Filter by category', 'all');
+    $categories = $this->getTermsFromVocabulary('article_category', false, 'Filter by category', 'all');
 
-        $form = \Drupal::formBuilder()->getForm('Drupal\efq\Form\FilterForm');
+    $form = \Drupal::formBuilder()->getForm('Drupal\efq\Form\FilterForm');
 
-        $form["container"] =  array(
-            '#prefix' => '<div class="filters filter-count-1">',
-            '#suffix' => '</div>',
-            'category' => array(
-                '#attributes' => [
-                    'id' => 'category',
-                    'class' => ['category','filter-list'],
-                ],
-                '#label_attributes' => [
-                    'for' => 'category'
-                ],
-                '#title' => t('Select a category'),
-                '#type' => 'select',
-                '#options' => $categories
-            )
-        );
+    $form["container"] =  array(
+      '#prefix' => '<div class="filters filter-count-1">',
+      '#suffix' => '</div>',
+      'category' => array(
+        '#attributes' => [
+          'id' => 'category',
+          'class' => ['category','filter-list'],
+        ],
+        '#label_attributes' => [
+          'for' => 'category'
+        ],
+        '#title' => t('Select a category'),
+        '#type' => 'select',
+        '#options' => $categories
+      )
+    );
 
 
-        return $form;
+    return $form;
 
-    }
+  }
 
 
 
